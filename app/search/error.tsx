719060a7f7d0e173483ed3fe96a9181670a8af81
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-05-12 00:02:33 +0500

    Fixed an error of Error page export

--- app/search/error.tsx
+++ app/search/error.tsx
@@ -2,10 +2,12 @@
 
 import { Box } from "@/components/Box";
 
-export const Error = () => {
+const Error = () => {
   return (
     <Box className="h-full flex items-center justify-center">
       <div className="text-neutral-400">Something went wrong!</div>
     </Box>
   );
 };
+
+export default Error;
